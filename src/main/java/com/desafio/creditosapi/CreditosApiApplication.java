052292c73861f98c28fe728ac4b5edee7d469415
lilzeldris <felipedantas1001@gmail.com>
2025-05-30 20:47:48 -0300

    chore: Configuração do ambiente e dependências

--- src/main/java/com/desafio/creditosapi/CreditosApiApplication.java
+++ src/main/java/com/desafio/creditosapi/CreditosApiApplication.java
@@ -2,10 +2,15 @@ package com.desafio.creditosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableKafka
+@EnableTransactionManagement
 public class CreditosApiApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CreditosApiApplication.class, args);
     }
-} 
+}
